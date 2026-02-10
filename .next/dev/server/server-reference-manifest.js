0ba269275557d32e26084b1c0ec36f4476dd5d48
Saad Mehmood <rajpootsaad491@gmail.com>
2026-02-11 00:45:39 +0500

    Image Tag Url Changed

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"QqeI4Ro3C56lWsQW0NsJMEf48wjCkqHY85vLMdWFVQs=\"\n}"
--- .next/dev/static/chunks/app_layout_1cf6b850.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__af6e68da._.css",
-  "static/chunks/_ba86202e._.js"
-],
-    source: "dynamic"
-});
